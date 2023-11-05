be4f2bca02762ae6cfc5e1e3fbdebbbbba0376b5
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-11-05 15:41:51 +0600

    User Hub Completed

---
+++ SignalR/Hubs/UserHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class UserHub:Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers",TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task<string> NewWindowLoaded()
+        {
+            TotalViews++;
+            //Send update to all the clients
+            await Clients.All.SendAsync("updateTotalViews",TotalViews);
+            return $"Total views - {TotalViews}";
+        }
+    }
+}
---
+++ SignalR/wwwroot/js/userCount.js
@@ -0,0 +1,35 @@
+﻿//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder()
+    //.configureLogging(signalR.LogLevel.Information)
+    .withAutomaticReconnect()
+    .withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build();
+//Connect to methods that hub invokes aka receives notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+//Invoke hub methods aka send notification to hub
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.invoke("NewWindowLoaded", "Siam").then((value) => console.log(value));
+}
+//start connection
+
+function fulfilled() {
+    //Do something
+    console.log("Connection to user hub successful");
+    newWindowLoadedOnClient();
+}
+function rejected() {
+    //do something
+    console.log("Connection to user hub rejected");
+
+}
+connectionUserCount.start().then(fulfilled, rejected);
